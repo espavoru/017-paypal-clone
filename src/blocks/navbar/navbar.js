bf42c3a8866a103f647d846116773569b81ca880
Olefirenko Oleg <espavoru@gmail.com>
2022-02-18 15:36:50 +0200

    Navbar dropdown (css & js)

---
+++ src/blocks/navbar/navbar.js
@@ -0,0 +1,15 @@
+const dropdownItems = document.querySelectorAll('.navbar__list-item--hover')
+
+dropdownItems.forEach(item => {
+  item.addEventListener('mouseover', () => {
+    item.lastElementChild.style.cssText = 'opacity: 1; visibility: visible;'
+
+    document.querySelector('.navbar__wrapper').style.background = 'linear-gradient(to right, #066399, #2f8fdf, #066399)'
+  })
+
+  item.addEventListener('mouseout', () => {
+    item.lastElementChild.style.cssText = 'opacity: 0; visibility: hidden;'
+
+    document.querySelector('.navbar__wrapper').style.background = 'none'
+  })
+})
