e883f503bf94415c2595ed5dc7f4bb9602b2a8fa
Olefirenko Oleg <espavoru@gmail.com>
2022-02-21 20:28:53 +0200

    Inner pages show on click logic

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,23 @@
-// const ready = require('./utils/documentReady.js');
+document.querySelectorAll('.logo').forEach(logo => {
+  logo.addEventListener('click', () => {
+    document.querySelector('.page__front-page').style.display = 'block'
+    document.querySelector('.login').style.display = 'none'
+    document.querySelector('.signup-page').style.display = 'none'
+  })
+})
 
-// ready(function(){
-//   console.log('DOM героически построен!');
-// });
+document.querySelectorAll('.btn--login').forEach(btnLogin => {
+  btnLogin.addEventListener('click', () => {
+    document.querySelector('.page__front-page').style.display = 'none'
+    document.querySelector('.login').style.display = 'flex'
+    document.querySelector('.signup-page').style.display = 'none'
+  })
+})
 
-// const $ = require('jquery');
-// $( document ).ready(function() {});
+document.querySelectorAll('.btn--signup').forEach(btnSignup => {
+  btnSignup.addEventListener('click', () => {
+    document.querySelector('.page__front-page').style.display = 'none'
+    document.querySelector('.login').style.display = 'none'
+    document.querySelector('.signup-page').style.display = 'block'
+  })
+})
